bfd39eda0db0b7de5d96b0ff8b075d8cb1a12e92
Ivan <soloveii@mydigicode.com>
2024-03-01 17:39:11 +0200

    test gcloud build

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Nest in da house';
+    return 'Hello World! Nest in da house! Testing the build process';
   }
 }
