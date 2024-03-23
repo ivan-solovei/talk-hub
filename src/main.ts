32ba5850905f5fa96aba6eafa3555f213f53aa90
Solovei Ivan <soloway.ivan.412@gmail.com>
2024-03-23 21:07:09 +0200

    added https options for server creation

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = process.env.PORT || 3000;
 
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/private-key.pem'),
+    cert: fs.readFileSync('./secrets/public-certificate.pem'),
+  }
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions
+  });
   app.enableCors();
   await app.listen(port);
   console.log(`Application is running on port: ${port}`);
