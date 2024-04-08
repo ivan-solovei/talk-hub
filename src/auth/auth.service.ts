73928bffb36baf1aae18651a48c29c3f2da57fe5
Solovei Ivan <soloway.ivan.412@gmail.com>
2024-04-09 01:51:35 +0300

    added try catch + added logger on requests

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, UnauthorizedException, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus, Logger } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService
@@ -13,9 +14,14 @@ export class AuthService {
     userName: string,
     password: string,
   ): Promise<{ userId: string, access_token: string }> {
-    const user: any = await this.usersService.findOne(userName);
+
+    const user = await this.findUser(userName);
+    
+    if(!user) {
+      throw new HttpException(`Can't find User with current name: ${userName}`, HttpStatus.NOT_FOUND);  
+    }
     if (user?.password !== password) {
-      throw new HttpException('Wrong password', HttpStatus.BAD_REQUEST);
+      throw new HttpException('Wrong password', HttpStatus.FORBIDDEN);
     }
     const payload = { sub: user.userId, userName: user.userName };
     return {
@@ -29,20 +35,40 @@ export class AuthService {
     email: string,
     password: string,
   ): Promise<{ user, access_token: string }> {    
-    const userNickName = await this.usersService.findOne(userName);
+    const userNickName = await this.findUser(userName);
     if(!!userNickName) {
-      throw new HttpException('User already exist', HttpStatus.BAD_REQUEST);
+      throw new HttpException('User already exist', HttpStatus.CONFLICT);
     }
-    const user = await this.usersService.create({
-        userName: userName,
-        email: email,
-        password: password,
-    })
+    const user = await this.createNewUser(userName, email, password)
+
+    if(!user) {
+      throw new HttpException(`Can't create User with current name: ${userName}`, HttpStatus.BAD_REQUEST);  
+    }
 
     const payload = { sub: user.userId, userName: user.userName };
     return {
       user,
       access_token: await this.jwtService.signAsync(payload),
     };
+  }
+
+  async findUser(userName) {
+    try {
+      return await this.usersService.findOne(userName)      
+    } catch (error) {
+      this.logger.log(`Ooops, can't find user: ${userName} in DB`)
+    }
+  }
+
+  async createNewUser(userName, email, password) {
+    try {
+      return await this.usersService.create({
+        userName: userName,
+        email: email,
+        password: password,
+    });
+    } catch (error) {
+      this.logger.log(`Ooops, can't create user: ${userName} in DB`)
+    }
   }
 }
--- src/transport/transport.service.ts
+++ src/transport/transport.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import axios, { AxiosHeaders } from "axios";
 import { ITransport } from "./interfaces/transport.interfaces";
 import { News } from "../AiProviders/News";
@@ -6,13 +6,12 @@ import { News } from "../AiProviders/News";
 
 @Injectable()
 export class TransportService implements ITransport {
+  private readonly logger = new Logger(TransportService.name);
     async call(url: string, params: AxiosHeaders): Promise<News | any> {
-        let response: any;
         try {
-            response = await axios.get(url, { headers: params });
+          return await axios.get(url, { headers: params });
           } catch (error) {
-            console.error(error);
+            this.logger.log('Ooops, failed to get news', error)
           }
-        return response?.data?.webPages?.value;
       }
 }
